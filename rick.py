08c58c337067d4398908c8acdc34b77d93fe8209
Caleb W <51061101+calebrwalk5@users.noreply.github.com>
2021-02-24 10:12:10 -0500

    Update rick.py
    secret rick roll

--- rick.py
+++ rick.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 
 client=commands.Bot(command_prefix="r?")
+client=commands.Bot(command_prefix="I'm")
 
 client.remove_command("help")
 
@@ -32,6 +33,19 @@ async def rick(ctx):
     embed.add_field(name="\u200b", value="Never gonna say goodbye", inline=True)
     embed.add_field(name="\u200b", value="Never gonna tell a lie and hurt you", inline=True)
     await ctx.send(embed=embed)
+    
+@client.command()
+@commands.cooldown(1, 30, commands.BucketType.user)
+async def never(ctx):
+    embed = discord.Embed(title="You now regret this", description="You have to wait to use this again", inline=True)
+    embed.add_field(name="\u200b", value="Never gonna give you up", inline=True)
+    embed.add_field(name="\u200b", value="Never gonna let you down", inline=True)
+    embed.add_field(name="\u200b", value="Never gonna run around and desert you", inline=True)
+    embed.add_field(name="\u200b", value="Never gonna make you cry", inline=True)
+    embed.add_field(name="\u200b", value="Never gonna say goodbye", inline=True)
+    embed.add_field(name="\u200b", value="Never gonna tell a lie and hurt you", inline=True)
+    await ctx.send(embed=embed)
+
 
 @rick.error
 async def rick_error(ctx, error):
